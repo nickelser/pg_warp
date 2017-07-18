7b7a293a4232d12eba86016b29f4610909985ca3
Lukas Fittl <lukas@fittl.com>
2017-07-18 12:18:18 -0700

    Cleanup error handling and progress reporting

--- main.go
+++ main.go
@@ -27,6 +27,10 @@ func truncateDestinationTables(destinationConn *pgx.Conn, tables map[string][]st
 	return nil
 }
 
+func printProgress(message string) {
+	fmt.Printf("%s %s\n", time.Now().Format(time.RFC822), message)
+}
+
 func makeInitialBackup(sourceURL string, destinationURL string, snapshotName string, parallelDump uint32, parallelRestore uint32, tmpDir string, includedTables []string, excludedTables []string) error {
 	dumpCommand := fmt.Sprintf("pg_dump --snapshot=%s -d %s", snapshotName, sourceURL)
 	restoreCommand := fmt.Sprintf("pg_restore --data-only --no-acl --no-owner -d %s", destinationURL)
@@ -48,7 +52,7 @@ func makeInitialBackup(sourceURL string, destinationURL string, snapshotName str
 		dumpCommand += fmt.Sprintf(" -Fd -j %d -f %s", parallelDump, dumpDir)
 		restoreCommand += fmt.Sprintf(" -Fd -j%d %s", parallelRestore, dumpDir)
 
-		fmt.Printf("Running initial backup dump (parallel)...\n")
+		printProgress("Running initial backup dump (parallel)...")
 
 		cmd := exec.Command("/bin/sh", "-c", dumpCommand)
 		cmd.Stdout = os.Stdout
@@ -58,7 +62,7 @@ func makeInitialBackup(sourceURL string, destinationURL string, snapshotName str
 			return fmt.Errorf("Error dumping data: %s\n", err)
 		}
 
-		fmt.Printf("Running initial backup restore (parallel)...\n")
+		printProgress("Running initial backup restore (parallel)...")
 
 		cmd = exec.Command("/bin/sh", "-c", restoreCommand)
 		cmd.Stdout = os.Stdout
@@ -73,7 +77,7 @@ func makeInitialBackup(sourceURL string, destinationURL string, snapshotName str
 			return fmt.Errorf("Could not remove dump directory: %s\n", err)
 		}
 	} else {
-		fmt.Printf("Running initial backup...\n")
+		printProgress("Running initial backup...")
 
 		dumpCommand += " -Fc"
 		cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("%s | %s", dumpCommand, restoreCommand))
@@ -142,17 +146,17 @@ func retrieveLagAndOutputStats(sourceConn *pgx.Conn, stats logStats, maxWal uint
 
 	err = sourceConn.QueryRow("SELECT COALESCE(pg_size_pretty(pg_xlog_location_diff(pg_current_xlog_flush_location(), replay_location)), 'n/a') FROM pg_stat_replication WHERE application_name = 'pg_warp'").Scan(&replicationLag)
 	if err != nil {
-		if strings.HasPrefix(err.Error(), "ERROR: function pg_current_xlog_flush_location() does not exist") {
+		if strings.HasPrefix(err.Error(), "ERROR: function pg_current_xlog_flush_location() does not exist") { // pre 9.6
 			replicationLag = "unknown"
 		} else {
-			fmt.Printf("Failed to retrieve replication lag: %s\n", err)
+			fmt.Printf("ERROR: Failed to retrieve replication lag: %s\n", err)
 			os.Exit(1)
 		}
 	}
 
-	// TODO: timestamp of last transaction replayed (after that COMMIT happened)
-	fmt.Printf("Stats: %0.2f TX/s, %0.2f inserts/s, %0.2f updates/s, %0.2f deletes/s, last LSN received: %s, replication lag: %s\n",
-		stats.txPerSecond(), stats.insertsPerSecond(), stats.updatesPerSecond(), stats.deletesPerSecond(), pgx.FormatLSN(maxWal), replicationLag)
+	// TODO: Add timestamp of last transaction replayed (after that COMMIT happened)
+	printProgress(fmt.Sprintf("Stats: %0.2f TX/s, %0.2f inserts/s, %0.2f updates/s, %0.2f deletes/s, last LSN received: %s, replication lag: %s",
+		stats.txPerSecond(), stats.insertsPerSecond(), stats.updatesPerSecond(), stats.deletesPerSecond(), pgx.FormatLSN(maxWal), replicationLag))
 }
 
 func startReplication(slotName string, replicationConn *pgx.ReplicationConn, sourceConn *pgx.Conn, destinationConn *pgx.Conn, tables map[string][]string, originName string, lastCommittedSourceLsn string) (chan bool, error) {
@@ -173,7 +177,7 @@ func startReplication(slotName string, replicationConn *pgx.ReplicationConn, sou
 		return stop, fmt.Errorf("Failed to start replication: %v", err)
 	}
 
-	fmt.Printf("Started replication\n")
+	printProgress("Started replication")
 
 	_, err = destinationConn.Exec(fmt.Sprintf("SELECT pg_replication_origin_session_setup('%s')", originName))
 	if err != nil {
@@ -211,7 +215,7 @@ func startReplication(slotName string, replicationConn *pgx.ReplicationConn, sou
 				message, err = replicationConn.WaitForReplicationMessage(standbyStatusInterval)
 				if err != nil {
 					if err == io.EOF {
-						fmt.Printf("End of stream (EOF), stopping replication\n")
+						fmt.Printf("ERROR: End of stream (EOF), stopping replication\n")
 						os.Exit(1)
 					}
 					if err != pgx.ErrNotificationTimeout {
@@ -229,14 +233,14 @@ func startReplication(slotName string, replicationConn *pgx.ReplicationConn, sou
 						// FIXME: Replace now() with the timestamp from the COMMIT message (when running with include-timestamps)
 						_, err = destinationConn.Exec(fmt.Sprintf("SELECT pg_replication_origin_xact_setup('%s', now()); SELECT txid_current();", pgx.FormatLSN(message.WalMessage.WalStart)))
 						if err != nil {
-							fmt.Printf("Could not setup replication origin for transaction on destination: %s", err)
+							fmt.Printf("ERROR: Could not setup replication origin for transaction on destination: %s", err)
 							os.Exit(1)
 						}
 					}
 
 					err = handleDecodingMessage(walString, destinationConn, tables, &stats)
 					if err != nil {
-						fmt.Printf("Failed to handle WAL message: %s\n", err)
+						fmt.Printf("ERROR: Failed to handle WAL message: %s\n", err)
 						os.Exit(1)
 					}
 
@@ -255,13 +259,13 @@ func startReplication(slotName string, replicationConn *pgx.ReplicationConn, sou
 
 				err = destinationConn.QueryRow("SELECT COALESCE(pg_replication_origin_session_progress(true), '0/0')").Scan(&lsnStr)
 				if err != nil {
-					fmt.Printf("Failed to retrieve replication origin progress from destination session: %s\n", err)
+					fmt.Printf("ERROR: Failed to retrieve replication origin progress from destination session: %s\n", err)
 					os.Exit(1)
 				}
 
 				lsn, err = pgx.ParseLSN(lsnStr)
 				if err != nil {
-					fmt.Printf("Failed to parse destination session progress LSN: %s\n", err)
+					fmt.Printf("ERROR: Failed to parse destination session progress LSN: %s\n", err)
 					os.Exit(1)
 				}
 
@@ -329,14 +333,14 @@ func main() {
 
 	sourceConnConfig, err := pgx.ParseURI(sourceURL)
 	if err != nil {
-		fmt.Printf("Could not parse source connection URI: %v\n", err)
+		fmt.Printf("ERROR: Could not parse source connection URI: %v\n", err)
 		return
 	}
 	sourceConnConfig.RuntimeParams["application_name"] = "pg_warp"
 
 	sourceConn, err := pgx.Connect(sourceConnConfig)
 	if err != nil {
-		fmt.Printf("Unable to establish source SQL connection: %v\n", err)
+		fmt.Printf("ERROR: Unable to establish source SQL connection: %v\n", err)
 		return
 	}
 	defer sourceConn.Close()
@@ -385,7 +389,7 @@ func main() {
 
 	destinationConnConfig, err := pgx.ParseURI(destinationURL)
 	if err != nil {
-		fmt.Printf("Could not parse destination connection URI: %v\n", err)
+		fmt.Printf("ERROR: Could not parse destination connection URI: %v\n", err)
 		return
 	}
 	// pgx misinterprets sslmode=require as sslmode=verify-full (and fails typically because it misses the CA)
@@ -393,7 +397,7 @@ func main() {
 	destinationConnConfig.RuntimeParams["application_name"] = "pg_warp"
 	destinationConn, err := pgx.Connect(destinationConnConfig)
 	if err != nil {
-		fmt.Printf("Unable to establish destination SQL connection: %v\n", err)
+		fmt.Printf("ERROR: Unable to establish destination SQL connection: %v\n", err)
 		return
 	}
 	defer destinationConn.Close()
@@ -401,20 +405,20 @@ func main() {
 	// Disable synchronous commit to optimize performance on EBS-backed databases
 	_, err = destinationConn.Exec("SET synchronous_commit = off")
 	if err != nil {
-		fmt.Printf("Could not disable synchronous_commit: %s\n", err)
+		fmt.Printf("ERROR: Could not disable synchronous_commit: %s\n", err)
 		return
 	}
 
 	// Disable Citus deadlock prevention to allow multi-shard modifications
 	_, err = destinationConn.Exec("SET citus.enable_deadlock_prevention = off")
 	if err != nil {
-		fmt.Printf("Could not disable citus.enable_deadlock_prevention: %s\n", err)
+		fmt.Printf("ERROR: Could not disable citus.enable_deadlock_prevention: %s\n", err)
 		return
 	}
 
 	replicationConn, err := pgx.ReplicationConnect(sourceConnConfig)
 	if err != nil {
-		fmt.Printf("Unable to establish source replication connection: %v\n", err)
+		fmt.Printf("ERROR: Unable to establish source replication connection: %v\n", err)
 		return
 	}
 	defer replicationConn.Close()
@@ -422,7 +426,7 @@ func main() {
 	// Support public.pg_replication_origin security definer functions on destination
 	_, err = destinationConn.Exec("SET search_path = public, pg_catalog")
 	if err != nil {
-		fmt.Printf("Could not set search path on destination: %s\n", err)
+		fmt.Printf("ERROR: Could not set search path on destination: %s\n", err)
 		return
 	}
 
@@ -431,19 +435,19 @@ func main() {
 	if resync || clean {
 		_, err = sourceConn.Exec(fmt.Sprintf("SELECT pg_drop_replication_slot('%s')", slotName))
 		if err != nil && !strings.HasPrefix(err.Error(), fmt.Sprintf("ERROR: replication slot \"%s\" does not exist", slotName)) {
-			fmt.Printf("Error dropping replication slot: %s\n", err)
+			fmt.Printf("ERROR: Could not drop replication slot: %s\n", err)
 			return
 		}
 
 		_, err = destinationConn.Exec(fmt.Sprintf("SELECT pg_replication_origin_drop('%s')", originName))
 		if err != nil && !strings.HasPrefix(err.Error(), fmt.Sprintf("ERROR: cache lookup failed for replication origin '%s'", originName)) {
-			fmt.Printf("Error dropping replication origin: %s\n", err)
+			fmt.Printf("ERROR: Could not drop replication origin: %s\n", err)
 			return
 		}
 	} else {
 		err = destinationConn.QueryRow(fmt.Sprintf("SELECT COALESCE(pg_replication_origin_progress('%s', true), '0/0')", originName)).Scan(&lastCommittedSourceLsn)
 		if err != nil && !strings.HasPrefix(err.Error(), "ERROR: cache lookup failed for replication origin") {
-			fmt.Printf("Failed to retrieve replication origin progress from destination: %s\n", err)
+			fmt.Printf("ERROR: Failed to retrieve replication origin progress from destination: %s\n", err)
 			os.Exit(1)
 		}
 	}
@@ -454,29 +458,29 @@ func main() {
 	}
 
 	if lastCommittedSourceLsn == "" {
-		fmt.Printf("Creating replication slot on source...\n")
-		// Create replication slot with "skip-empty-xacts", "include-timestamps" option
+		printProgress("Creating replication slot on source...")
+		// TODO: Create replication slot with "skip-empty-xacts", "include-timestamps" option
 		var snapshotName string
 		snapshotName, err = replicationConn.CreateReplicationSlotAndGetSnapshotName(slotName, "test_decoding")
 		if err != nil {
-			fmt.Printf("replication slot create failed: %v\n", err)
+			fmt.Printf("ERROR: Could not create replication slot: %s\n", err)
 			return
 		}
 
 		if !skipInitialSync {
-			fmt.Printf("Truncating destination tables...\n")
+			printProgress("Truncating destination tables...")
 			err = truncateDestinationTables(destinationConn, tables)
 			if err != nil {
-				fmt.Printf("Destination truncate failed: %s\n", err)
+				fmt.Printf("ERROR: Destination truncate failed: %s\n", err)
 				return
 			}
 
 			err = makeInitialBackup(sourceURL, destinationURL, snapshotName, parallelDump, parallelRestore, tmpDir, []string(includedTables), []string(excludedTables))
 			if err != nil {
-				fmt.Printf("Initial backup failed: %s\n", err)
+				fmt.Printf("ERROR: Initial backup failed: %s\n", err)
 				err = replicationConn.DropReplicationSlot(slotName)
 				if err != nil {
-					fmt.Printf("Error dropping replication slot: %s\n", err)
+					fmt.Printf("ERROR: Could not drop replication slot: %s\n", err)
 				}
 				return
 			}
@@ -488,7 +492,7 @@ func main() {
 			return
 		}
 	} else {
-		fmt.Printf("Resuming previous operation for slot %s, last committed source LSN: %s\n", slotName, lastCommittedSourceLsn)
+		printProgress(fmt.Sprintf("Resuming previous operation for slot %s, last committed source LSN: %s", slotName, lastCommittedSourceLsn))
 	}
 
 	sigs := make(chan os.Signal, 1)
